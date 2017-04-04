6537d35f99c02af6abf20f6b05bca7d3c6221856
liuhui <liuhuijisuanji@gmail.com>
2017-04-04 17:04:47 +0800

    use before_action to find_team_and_check_permission

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,6 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_team_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @teams = Team.all
@@ -21,21 +22,9 @@ class TeamsController < ApplicationController
   end
 
   def edit
-    @team = Team.find(params[:id])
-
-    if current_user != @team.user
-      redirect_to root_path, alert: "You have no permission"
-    end
-
   end
 
   def update
-    @team = Team.find(params[:id])
-
-    if current_user != @team.user
-      redirect_to root_path, alert: "You have no permission"
-    end
-
     if @team.update(team_params)
       redirect_to :teams
     else
@@ -44,12 +33,6 @@ class TeamsController < ApplicationController
   end
 
   def destroy
-    @team = Team.find(params[:id])
-
-    if current_user != @team.user
-      redirect_to root_path, alert: "You have no permission"
-    end
-
     @team.destroy
     redirect_to :teams
   end
@@ -63,4 +46,13 @@ class TeamsController < ApplicationController
   def team_params
     params.require(:team).permit(:title, :description)
   end
+
+  def find_team_and_check_permission
+    @team = Team.find(params[:id])
+
+    if current_user != @team.user
+      redirect_to root_path, alert: "You have no permission"
+    end
+  end
+
 end
